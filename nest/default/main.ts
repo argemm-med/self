f4a5733303cf93e823aa3ec4f644d67482d01d93
argemm <argemm7@gmail.com>
2023-06-11 16:52:33 +0600

    add user resource

--- nest/default/main.ts
+++ nest/default/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as process from 'process'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  try {
+    const PORT = 3000 || process.env.port
+
+    const app = await NestFactory.create(AppModule)
+    await app.listen(PORT)
+    console.log(`Application started on port: http://localhost:${PORT}`)
+  } catch (err) {
+    console.error('Application start error: ', err.message)
+  }
 }
 
 bootstrap()
